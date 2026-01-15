ac91b644196a133c7a002c78e5220934191196a6
Will Warner <47200097+cofad@users.noreply.github.com>
2026-01-15 05:03:35 +0000

    chore: remove CSS files from test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
       provider: 'v8',
       reporter: ['text', 'lcov'],
       include: ['src/*'],
+      exclude: ['src/*.css'],
       thresholds: {
         statements: 80,
         branches: 80,
