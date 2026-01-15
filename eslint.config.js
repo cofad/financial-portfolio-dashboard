7566e42b2603ffe89c39c300be8fcd6780950df4
Will Warner <47200097+cofad@users.noreply.github.com>
2026-01-15 05:48:32 +0000

    chore: remove coverage directory from linting

--- eslint.config.js
+++ eslint.config.js
@@ -8,7 +8,7 @@ import reactX from 'eslint-plugin-react-x';
 import reactDom from 'eslint-plugin-react-dom';
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'coverage']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
