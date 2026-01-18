bba5a4e792bd73fdcbd975a7a34def83d756a713
Will Warner <47200097+cofad@users.noreply.github.com>
2026-01-18 23:06:06 +0000

    feat: update to use finnhub proxy server

--- src/services/finnhub/finnhub.ts
+++ src/services/finnhub/finnhub.ts
@@ -17,18 +17,16 @@ const symbolLookupResponseSchema = z.object({
 export type SymbolLookupResult = z.infer<typeof symbolLookupResultSchema>;
 export type SymbolLookupResponse = z.infer<typeof symbolLookupResponseSchema>;
 
-const FINNHUB_BASE_URL = 'https://finnhub.io/api/v1';
-const CORS_ANYWHERE_BASE_URL = 'https://cors-anywhere.herokuapp.com/';
+const FINNHUB_PROXY = 'https://financial-portfolio-dashboard-server.fly.dev/api/v1';
 
 const finnhubClient = axios.create({
-  baseURL: `${CORS_ANYWHERE_BASE_URL}${FINNHUB_BASE_URL}`,
+  baseURL: FINNHUB_PROXY,
 });
 
 export const symbolLookup = async (query: string): Promise<SymbolLookupResponse> => {
   const response = await finnhubClient.get<unknown>('/search', {
     params: {
       q: query,
-      token: env.VITE_FINNHUB_API_KEY,
     },
   });
 
