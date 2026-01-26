31389b12624c4197ab9fb860cc95148021bded06
cofad <will.r.warner@gmail.com>
2026-01-25 21:05:48 -0600

    chore: set time for tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
     environment: 'jsdom',
+    setupFiles: ['./vitest.setup.ts'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'lcov'],
---
+++ vitest.setup.ts
@@ -0,0 +1,2 @@
+// eslint-disable-next-line no-restricted-syntax
+process.env.TZ = 'America/Chicago';
