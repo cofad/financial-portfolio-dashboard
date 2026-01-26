d46153d3fc43779df33f621ab74133044b7fd118
cofad <will.r.warner@gmail.com>
2026-01-25 22:07:48 -0600

    chore: remove path to utilize new subdomain URL

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   // Path for GitHub Pages deployment
-  base: '/financial-portfolio-dashboard/',
+  // base: '/financial-portfolio-dashboard/',
   plugins: [react(), tailwindcss()],
   resolve: {
     tsconfigPaths: true,
